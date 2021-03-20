de73a07e29c17bf638ffba14c0d07b956f15af39
Matheus Silva <msilvadev@Matheuss-MacBook-Pro.local>
2021-03-20 17:46:42 -0300

    baseurl from aws services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ec2-3-12-123-96.us-east-2.compute.amazonaws.com:8091'
+  baseUrl: 'http://ec2-3-141-195-206.us-east-2.compute.amazonaws.com:8091'
 };
